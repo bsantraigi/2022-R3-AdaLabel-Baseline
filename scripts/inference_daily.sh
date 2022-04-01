c53dced176f2fb0529bd6cb16bd2612bbd0b6d7b
Ravi Ghadia <40660742+ghadiaravi13@users.noreply.github.com>
2022-04-02 00:01:46 +0530

    updated paths

--- scripts/inference_daily.sh
+++ scripts/inference_daily.sh
@@ -1,7 +1,8 @@
-DATA_DIR="data_daily"
+DATA = "data_daily"
+DATA_DIR="content/data/data_daily"
 #DATA_DIR="data_ost"
 DATASET="bert"
 
 python3 translate.py -gpu "$1" -model "$2" \
-    -output result/"$DATASET"_adalab_"$DATA_DIR".txt -beam 1 -batch_size 128 \
+    -output result/"$DATASET"_adalab_"$DATA".txt -beam 1 -batch_size 128 \
     -src "$DATA_DIR"/src-test.txt -max_length 30 -tokenizer bert
